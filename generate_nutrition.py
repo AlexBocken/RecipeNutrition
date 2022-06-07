d69a15efceae0376b26e637758ba14fb86d4a9fa
AlexBocken <alexander@bocken.org>
2022-06-07 07:06:39 +0200

    wait for login to complete

--- generate_nutrition.py
+++ generate_nutrition.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
 import os
 import subprocess
 
@@ -24,29 +25,29 @@ def get_login_credentials():
     return email_login, password_login
 
 def login_to_cronometer(email_login, password_login):
-    driver.get("https://cronometer.com")
-    login_link = driver.find_element(by=By.CSS_SELECTOR, value="a.link__hero-login")
-    login_link.click()
+    driver.get("https://cronometer.com/login/")
     email_input = driver.find_element(by=By.CSS_SELECTOR, value="input#usernameBox.textbox--1.login-fields")
     password_input = driver.find_element(by=By.CSS_SELECTOR, value="input#password.textbox--1.login-fields")
     email_input.send_keys(email_login)
     password_input.send_keys(password_login)
     button = driver.find_element(by=By.CSS_SELECTOR, value="button#login-button.submit--1.login-fields")
     button.click()
+    WebDriverWait(driver, timeout=10).until(lambda d: d.title != "Cronometer Login")
+
 
 email_login, password_login = get_login_credentials()
 
-driver = webdriver.Chrome()
-options = Options()
+chrome_options = Options()
+chrome_options.add_argument('--force-device-scale-factor=1.5')
+chrome_options.page_load_strategy = 'normal'
 
+driver = webdriver.Chrome(options=chrome_options)
 #works for login, consider slowing down for other parts or using waits
 #https://www.selenium.dev/documentation/webdriver/capabilities/shared/#pageloadstrategy
 #https://www.selenium.dev/documentation/webdriver/waits/
-options.page_load_strategy = 'none'
 login_to_cronometer(email_login, password_login)
-options.page_load_strategy = 'normal' #TODO, temp remove if possible
 
-foods_link = driver.find_element(by=By.LINK_TEXT, value="Foods")
-foods_link.click()
-add_recipe = driver.find_element(by=By.NAME, value="+ Add Recipe")
+driver.get("https://cronometer.com/#foods")
+add_recipe = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.XPATH, value="//button[text()='+ Add Recipe']"))
+add_recipe.click()
 #print(test)
