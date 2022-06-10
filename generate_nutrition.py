20d15283021c43be7ca734c0877683cf934f1311
Alexander Bocken <32177905+AlexBocken@users.noreply.github.com>
2022-06-10 10:28:26 +0200

    improved match_unit() (#6)
    * improved match_unit()
    
    * integrated func as its only used by one other func
    
    * remove pyright warning
    
    * better matching for custom portion sizes
    
    * increase reliability considerably
    
    * more units
    
    * fix manual entry if Add button is clicked

--- generate_nutrition.py
+++ generate_nutrition.py
@@ -5,8 +5,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import *
+from selenium.common.exceptions import * # pyright: ignore
 from time import sleep
+import re
 import os
 import subprocess
 from datetime import date
@@ -38,32 +39,99 @@ def login_to_cronometer(email_login, password_login):
     password_input.send_keys(password_login)
     button = driver.find_element(by=By.CSS_SELECTOR, value="button#login-button.submit--1.login-fields")
     button.click()
-    WebDriverWait(driver, timeout=10).until(lambda d: d.title != "Cronometer Login")
+    WebDriverWait(driver, timeout=60).until(lambda d: d.title != "Cronometer Login")
+
+def adjust_amount_by_multiplier(amount, unit, select_text):
+    '''
+    returns adjusted amount for given option. Can detect fractionals
+    (e.g. adjust_amount_by_multiplier(3, 'tbsp', '1/4 tbsp - 0.5 g') returns 12
+    amount: amount of initial unit (e.g. 300 for 300 mL)
+    unit: base unit to match on cronometer.com such as "tbsp". NOT unit used in recipe
+    select_text: whole option displayed such as "1/4 tbsp - 0.5 g"
+    '''
+    amount_unit = re.sub(f'[^0-9]*([0-9]+/)?([0-9]+)?\\s?{unit}.*', '\\1\\2', select_text)
+    mul = re.sub(f'(.*){unit}', '\\1', amount_unit)
+    if not mul:
+        mul = '1'
+    if '/' in mul:
+        n = float( re.sub('([0-9])*/([0-9])*', '\\1', mul) )
+        d = float( re.sub('([0-9])*/([0-9])*', '\\2', mul) )
+        mul = n / d
+    else:
+        mul = float(mul)
+    if(mul):
+        amount /= mul
+    return amount
 
 def match_unit(select_list, amount, unit):
-    if(unit == 'g'):
+    '''matches personal unit convention from recipe with cronometer options
+    if necessary, amount get's adjusted to compensate for multipliers found in cronometer unit
+    '''
+    if unit == 'g':
         for el in select_list:
-            if el.text == 'g':
+            if 'g' == el.text:
                 return amount, el.text
-    elif(unit == 'EL'):
         for el in select_list:
-            if 'tbsp' in el.text: #TODO: check whether fraction units of tbsp exist
+            if 'g' in el.text:
+                amount = adjust_amount_by_multiplier(amount, 'g', el.text)
                 return amount, el.text
-    elif(unit == 'TL'):
+    if unit == 'kg':
+        return match_unit(select_list, 1000*amount, 'g')
+    if unit == 'dkg':
+        return match_unit(select_list, 100*amount, 'g')
+    elif( re.search('m(l|L)', unit) ):
         for el in select_list:
-            if 'tsp' in el.text: #TODO: check whether fraction units of tsp exist
+            if (unit_match := re.search('m(l|L)', el.text) ):
+                amount = adjust_amount_by_multiplier(amount, unit_match.group(0) , el.text)
                 return amount, el.text
+        for el in select_list:
+            if( re.search('cup (-|—)', el.text) ):
+                one_cup_ml = 236.5882365
+                amount /= one_cup_ml
+                #TODO: probably is "half" or "quarter" cup, not "0.5", "0.25" cups
+                #TODO: maybe like tbsp '1/4', '1/2'?
+                amount = adjust_amount_by_multiplier(amount, 'cup', el.text)
+                return amount, el.text
+    elif( re.search( '(medium(-sized)?|mittel(gro(ss|ß))?)', unit) ):
+            for el in select_list:
+                if( re.search('medium (-|—)', el.text) ):
+                    return amount, el.text
+    elif( re.search( '(very small|sehr klein)', unit) ):
+            for el in select_list:
+                if( re.search('very small (-|—)', el.text) ):
+                    return amount, el.text
+    elif( re.search( '(small|klein)', unit) ):
+            for el in select_list:
+                if( re.search('small (-|—)', el.text) ):
+                    return amount, el.text
+    elif( re.search( '(large(-sized)?|gro(ss|ß))', unit) ):
+            for el in select_list:
+                if( re.search('large (-|—)', el.text) ):
+                    return amount, el.text
+    elif( re.search( '(EL|tbsp)', unit) ):
+        for el in select_list:
+            if( re.search('tbsp (-|—)', el.text) ):
+                amount = adjust_amount_by_multiplier(amount, 'tbsp', el.text)
+                return amount, el.text
+    elif( re.search( '(TL|tsp)', unit) ):
+        for el in select_list:
+            if( re.search('tsp (-|—)', el.text) ):
+                amount = adjust_amount_by_multiplier(amount, 'tsp', el.text)
+                return amount, el.text
+    elif (re.search('c(l|L)', unit)):
+        return match_unit(select_list, 10*amount, 'mL')
+    elif (re.search('d(l|L)', unit)):
+        return match_unit(select_list, 100*amount, 'mL')
+    # needs to be last non-generic match as it is quite generic to just look for 'l'
+    elif (re.search('(l|L)', unit)):
+        return match_unit(select_list, 1000*amount, 'mL')
     # generic catcher
-    else:
-        for el in select_list:
-            if el.text == unit:
-                return amount, el.text
-        for el in select_list:
-            #fuzzier, more unprecise, matching as an absolute fallback
-            if unit in el.text:
-                return amount, el.text
-        print("Found no matching unit, please select proper unit and amount manually")
-        return None, None
+    for el in select_list:
+        if el.text == unit:
+            amount = adjust_amount_by_multiplier(amount, unit, el.text)
+            return amount, el.text
+    print("Found no matching unit, please select proper unit and amount manually")
+    return None, None
 
 def add_ingredient(amount, unit, ingredient):
     '''Assumes that it starts on recipe page and adds one ingredient
@@ -74,33 +142,47 @@ def add_ingredient(amount, unit, ingredient):
     '''
     print(f'Trying to add {amount} {unit} of {ingredient}')
 
+    add_ingredient_xpath_expr = "//img[@title='Add Ingredient']"
     try:
         #go to ingredient popup
-        driver.find_element(By.XPATH, value="//img[@title='Add Ingredient']").click()
+        driver.find_element(By.XPATH, value=add_ingredient_xpath_expr).click()
     except ElementClickInterceptedException:
         remove_cookie_banner()
-        driver.find_element(By.XPATH, value="//img[@title='Add Ingredient']").click()
+        driver.find_element(By.XPATH, value=add_ingredient_xpath_expr).click()
+    # Weird exception... "Frame detached". Probably too fast for unreliable internet?
+    except WebDriverException:
+        sleep(2)
+        driver.find_element(By.XPATH, value=add_ingredient_xpath_expr).click()
 
     #search and wait for results to load
     search = driver.find_element(By.XPATH, value='//div[@class="popupContent"]//div/img/following-sibling::input')
     search.send_keys(ingredient)
     spinner_xpath_expr = '//div[@class="popupContent"]//img[@src="img/spin2.gif"]'
-    driver.find_element(By.XPATH, value='//div[@class="popupContent"]//button[text()="Search"]').click()
+    btn_search = driver.find_element(By.XPATH, value='//div[@class="popupContent"]//button[text()="Search"]')
+    btn_search.click()
+    first_result_xpath_expr = '//div[@class="popupContent"]//tr[@class="prettyTable-header"]/following-sibling::tr[1]/td[1]/div[@class="gwt-Label"]'
     try:
        WebDriverWait(driver, timeout=2).until(EC.visibility_of_element_located((By.XPATH, spinner_xpath_expr)))
-       WebDriverWait(driver, timeout=2).until(EC.invisibility_of_element_located((By.XPATH, spinner_xpath_expr)))
-    except (NoSuchElementException, TimeoutException) as e:
+       WebDriverWait(driver, timeout=30).until(EC.invisibility_of_element_located((By.XPATH, spinner_xpath_expr)))
+    except (NoSuchElementException, TimeoutException):
+        print("Waiting...")
         sleep(2) #could be cleaner, let's be real, doesn't have to be
         pass
-    #click on first result
-    first_result = driver.find_element(By.XPATH, value='//div[@class="popupContent"]//tr[@class="prettyTable-header"]/following-sibling::tr[1]/td[1]/div[@class="gwt-Label"]')
-    first_result.click()
+    try:
+        first_result = driver.find_element(By.XPATH, value=first_result_xpath_expr)
+        first_result.click()
+    except NoSuchElementException:
+        print("First result still not found. Your internet connection might be unstable. Consider trying at another time.")
+        btn_search.click()
+        sleep(10)
+        first_result = driver.find_element(By.XPATH, value=first_result_xpath_expr)
+        first_result.click()
     found_ingredient_name = first_result.text
     #wait for amount and unit input to appear
     unit_el_xpath_expr = '//div[text()="Serving:"]/following-sibling::div//div[@class="select-pretty"]/select[@class="gwt-ListBox"]'
-    WebDriverWait(driver, timeout=10).until( EC.presence_of_element_located( (By.XPATH, unit_el_xpath_expr) ) )
+    WebDriverWait(driver, timeout=60).until( EC.presence_of_element_located( (By.XPATH, unit_el_xpath_expr) ) )
     unit_el = driver.find_element(By.XPATH, value=unit_el_xpath_expr)
-    WebDriverWait(driver, timeout=10).until( EC.element_to_be_clickable(unit_el) )
+    WebDriverWait(driver, timeout=60).until( EC.element_to_be_clickable(unit_el) )
     unit_select_object = Select(unit_el)
     options = unit_select_object.options
 
@@ -110,9 +192,9 @@ def add_ingredient(amount, unit, ingredient):
     if(amount is None):
         print("Please press enter in this window when manual entry is done.", end='')
         input()
-        unit = unit_select_object.first_selected_option.text
-        amount = amount_el.get_attribute("value")
         try:
+            amount = float(amount_el.get_attribute("value"))
+            unit = unit_select_object.first_selected_option.text
             add_button.click()
             print(f'Added {amount} * "{unit}" of "{found_ingredient_name}"')
         except ElementClickInterceptedException:
@@ -130,6 +212,8 @@ def add_ingredient(amount, unit, ingredient):
         add_button.click()
         print(f'Added {amount} * "{element_text}" of "{found_ingredient_name}"')
     except ElementClickInterceptedException:
+        amount = float(amount_el.get_attribute("value"))
+        unit = unit_select_object.first_selected_option.text
         remove_cookie_banner()
         add_ingredient(amount, unit, ingredient)
 
@@ -143,9 +227,9 @@ def add_recipe(name, servings, ingredients):
        servings: int amount of servings in recipe
        ingredients: list of three-tuple (amount, unit, ingredient)
     '''
-    add_recipe = WebDriverWait(driver, timeout=10).until(lambda d: d.find_element(By.XPATH, value="//button[text()='+ Add Recipe']"))
+    add_recipe = WebDriverWait(driver, timeout=60).until(lambda d: d.find_element(By.XPATH, value="//button[text()='+ Add Recipe']"))
     add_recipe.click()
-    change_meta_data(titel)
+    change_meta_data(name)
     try:
         change_servings(servings)
     except ElementClickInterceptedException:
@@ -160,11 +244,9 @@ def add_recipe(name, servings, ingredients):
 def change_meta_data(recipe_name):
     today = date.today()
     name_box = driver.find_element(By.XPATH, '//input[@title="New Recipe"]')
-    name_box.click()
     name_box.clear()
     name_box.send_keys(recipe_name)
     notes_box = driver.find_element(By.XPATH,'//textarea[@class="gwt-TextArea"]')
-    notes_box.click()
     notes_box.send_keys(f"Added on {today.strftime('%d.%m.%Y')}")
 
 def change_servings(servings):
@@ -207,4 +289,5 @@ if(__name__ == "__main__"):
     login_to_cronometer(email_login, password_login)
 
     driver.get("https://cronometer.com/#foods")
-    add_recipe(titel, servings, [ (3, "TL", "Pfefferminz"), (10, "ml", "Milch"), (200, "ml", "Wasser") ])
+    add_recipe(titel, servings, [ (2, 'dl', 'MinusL, Milch, Proteingehalt, 0.9%'), (2, "mittelgroß", "Apfel"), (3, "TL", "Pfefferminz"), (100, "ml", "Milch"), (200, "ml", "Wasser"), (1, 'kg', 'Mehl, weiss'), (1, 'dkg', 'Dm Bio, Hafer Joghurt, Natur')])
+#    driver.quit()
